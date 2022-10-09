feb59b382d4b01929a4c6ef57d4cd6575c9cb067
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-09 10:25:02 +0600

    done added pricing calculation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,21 +7,30 @@ const Cart = ({cart}) => {
   let price = 0;
   let shipping = 0;
   let quantity = 0
-  
+
+  for(const product of cart){
+    quantity= quantity+product.quantity
+     price = price + product.price* product.quantity
+     shipping = shipping + product.shipping*  product.quantity
+  }
+
+  const tax =parseFloat((price *.1).toFixed(2))
+  const grandTotal = tax+price+shipping
+
     return (
         <div className='cart'>
         <div className='full-cart'>
         <div className='calc'>
         <h2 >Order Summary</h2>  
         <hr />
-         <h5>Selected Items :</h5>
+         <h5>Selected Items :{quantity}</h5>
         
      
-          <h5>Total Price :</h5>
+          <h5>Total Price :{price}</h5>
        
-         <h5>Total Shipping Charge :</h5>
-         <h5>TAX :</h5>
-         <h3>Grand Total:</h3>
+         <h5>Total Shipping Charge :{shipping}</h5>
+         <h5>TAX :{tax}</h5>
+         <h3>Grand Total:{grandTotal}</h3>
         </div>
         <div className="btn-group">
           <button className='btn-cart bg-orange'>Review Order <span className='icon'> <FontAwesomeIcon icon={faArrowRight}/></span> </button>

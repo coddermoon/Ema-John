19168d4d336560a8fc27d02f8e5bca27fb555938
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-09 12:14:24 +0600

    local storage added doen

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { getStoredCart } from '../../assets/utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Products from '../Products/Products';
 
@@ -8,6 +9,26 @@ import './shop.css'
 const Shop = () => {
   const[cart,setCart]=useState([])
   const products = useLoaderData()
+
+  useEffect(()=>{
+
+    const storedCart = getStoredCart()
+    let savedCart = []
+    for (const id in storedCart) {
+      
+  
+     const addedProduct = products.find(product=>id ===product.id)
+     if(addedProduct){
+      const quantity = storedCart[id]
+      addedProduct.quantity=quantity
+      
+      savedCart.push(addedProduct)
+     }
+     
+    }
+    setCart(savedCart)
+  
+  },[products])
 
   // handle add to Cart
 

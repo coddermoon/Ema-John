b534e071b7d283179d07e0c23667a0220a3e8f74
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-18 12:58:31 +0600

    login form design done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './Login.css'
+import React from "react";
+import "./Login.css";
 
 const Login = () => {
-    return (
-        <div>
-           <h2>login</h2> 
+  return (
+    <div className="form-space">
+        <div className="form-container">
+      <h2 className="form-title">Login</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="email Adress" />
         </div>
-    );
+        <div className="form-control">
+          <label htmlFor="password">password</label>
+          <input type="password" name="password" placeholder="password " />
+        </div>
+       <div className="form-control">
+       <input className="btn-submit" type="submit" value="Login" />
+       </div>
+      </form>
+    </div>
+    </div>
+  );
 };
 
 export default Login;

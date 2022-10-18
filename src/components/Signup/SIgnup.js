f110c1838861f25f9194634b52bc58194dc62d47
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-18 16:15:52 +0600

    user created successfully

--- src/components/Signup/SIgnup.js
+++ src/components/Signup/SIgnup.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/UserContext';
 import './signup.css'
 
 const SIgnup = () => {
+    const {createUser} = useContext(AuthContext)
+    const handleCreateUser=(e)=>{
+        e.preventDefault();
+        const form= e.target 
+        const email = form.email.value 
+        const password = form.password.value 
+        // user created function
+        createUser(email,password)
+        .then(result=>{
+            console.log(result)
+            alert("successfully user created")
+        })
+        .catch(error=>{
+            alert(error.message)
+        })
+    }
+    
     return (
-        <div>
-            <h2>this is sign up</h2>
+        <div className="form-space">
+        <div className="form-container">
+      <h2 className="form-title">Register Now</h2>
+      <form onSubmit={handleCreateUser}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="email Adress" required/>
         </div>
+        <div className="form-control">
+          <label htmlFor="password">password</label>
+          <input type="password"  name="password" placeholder="password "  required/>
+        </div>
+       <div className="form-control">
+       <input className="btn-submit" type="submit" value="Register" />
+       </div>
+      </form>
+    </div>
+    </div>
     );
 };
 
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,11 +1,30 @@
 import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import app from '../assets/utilities/firebase.config';
 
-const AuthContext = createContext()
+
+export const AuthContext = createContext()
+
+// called auth
+
+const auth = getAuth(app)
+
+
+
 
 const UserContext = ({children}) => {
-    const authInfo = {}
+
+    // signup functionalities
+
+const createUser = (email,password)=>{
+
+    return  createUserWithEmailAndPassword(auth,email,password)
+  
+  }
+
+    const authInfo = {createUser}
     return (
-        <AuthContext.Provider >
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
